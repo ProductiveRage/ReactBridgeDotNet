c03822b94c4a9474d665adb8946996cdcde0667e
DanRoberts <742348+ProductiveRage@users.noreply.github.com>
2015-07-21 18:36:12 +0100

    The "Name" attributes on FormEvent and FormEvent<T> were not required since the property name is already "target" in both cases and Bridge does not attempt to force them to have unique names (since they are effectively unique properties on different classes) due to the "Ignore" attribute on the classes (this is different to DuoCode where both properties DID require some guidance in naming))

--- Bridge.React/Events/FormEvent.cs
+++ Bridge.React/Events/FormEvent.cs
@@ -3,14 +3,12 @@
     [Ignore]
     public class FormEvent : SyntheticEvent
     {
-        [Name("target")]
         public EventTarget target;
     }
 
     [Ignore]
     public class FormEvent<T> : FormEvent where T : EventTarget
     {
-        [Name("target")]
         public new T target;
     }
 }

0734f4047ecdfb9db3b0f01134d39c846b51e0a4
DanRoberts <DanRoberts@localhost>
2015-11-03 19:38:41 +0000

    Broke the view model class out of the SimpleExampleStore so that, despite the extra file, things are clearer

--- BridgeExamples/Stores/SimpleExampleStore.cs
+++ BridgeExamples/Stores/SimpleExampleStore.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Bridge.Html5;
 using Bridge.React;
 using BridgeExamples.Actions;
@@ -10,47 +6,47 @@ using BridgeExamples.Components;
 
 namespace BridgeExamples.Stores
 {
-    public class SimpleExampleStore
-    {
+	public class SimpleExampleStore
+	{
         private readonly Element _renderContainer;
-        private readonly AppDispatcher _dispatcher;
-        private ViewModel _viewModel;
+		private readonly AppDispatcher _dispatcher;
+        private SimpleExampleStoreViewModel _viewModel;
         public SimpleExampleStore(Element renderContainer, AppDispatcher dispatcher)
-        {
+		{
             if (renderContainer == null)
                 throw new ArgumentNullException("renderContainer");
-            if (dispatcher == null)
-                throw new ArgumentNullException("dispatcher");
+			if (dispatcher == null)
+				throw new ArgumentNullException("dispatcher");
 
             _renderContainer = renderContainer;
-            _dispatcher = dispatcher;
-            _viewModel = new ViewModel(lastUpdated: DateTime.Now, message: "Hi!", validationError: "");
+			_dispatcher = dispatcher;
+			_viewModel = new SimpleExampleStoreViewModel(lastUpdated: DateTime.Now, message: "Hi!", validationError: "");
             RenderIfActive();
 
-            _dispatcher.Register(message =>
-            {
-                var recordChange = message.Action as RecordChangeAction<ViewModel>;
-                if (recordChange != null)
-                {
-                    UserEdit(recordChange.Value);
-                    return;
-                }
+			_dispatcher.Register(message =>
+			{
+				var recordChange = message.Action as RecordChangeAction<SimpleExampleStoreViewModel>;
+				if (recordChange != null)
+				{
+					UserEdit(recordChange.Value);
+					return;
+				}
 
-                if (message.Action is TimePassedAction)
-                {
-                    _viewModel = new ViewModel(lastUpdated: DateTime.Now, message: _viewModel.Message, validationError: _viewModel.ValidationError);
+				if (message.Action is TimePassedAction)
+				{
+					_viewModel = new SimpleExampleStoreViewModel(lastUpdated: DateTime.Now, message: _viewModel.Message, validationError: _viewModel.ValidationError);
                     RenderIfActive();
-                    return;
-                }
-            });
-        }
+					return;
+				}
+			});
+		}
 
-        private void UserEdit(ViewModel viewModel)
-        {
-            if (viewModel == null)
-                throw new ArgumentNullException("viewModel");
+		private void UserEdit(SimpleExampleStoreViewModel viewModel)
+		{
+			if (viewModel == null)
+				throw new ArgumentNullException("viewModel");
 
-            _viewModel = viewModel;
+			_viewModel = viewModel;
             RenderIfActive();
         }
 
@@ -77,39 +73,9 @@ namespace BridgeExamples.Stores
             if (message == null)
                 throw new ArgumentNullException("message");
 
-            _dispatcher.HandleViewAction(new RecordChangeAction<ViewModel>(
-                new ViewModel(lastUpdated: DateTime.Now, message: message, validationError: (message.Trim() == "") ? "Why no message??" : "")
+			_dispatcher.HandleViewAction(new RecordChangeAction<SimpleExampleStoreViewModel>(
+				new SimpleExampleStoreViewModel(lastUpdated: DateTime.Now, message: message, validationError: (message.Trim() == "") ? "Why no message??" : "")
             ));
-        }
-
-        private class ViewModel
-        {
-            public ViewModel(DateTime lastUpdated, string message, string validationError)
-            {
-                if (message == null)
-                    throw new ArgumentNullException("message");
-                if (validationError == null)
-                    throw new ArgumentNullException("validationError");
-
-                LastUpdated = lastUpdated.ToString("HH:mm:ss");
-                Message = message;
-                ValidationError = validationError;
-            }
-
-            /// <summary>
-            /// This will never be null
-            /// </summary>
-            public string LastUpdated { get; private set; }
-
-            /// <summary>
-            /// This will never be null
-            /// </summary>
-            public string Message { get; private set; }
-
-            /// <summary>
-            /// This will never be null
-            /// </summary>
-            public string ValidationError { get; private set; }
-        }
-    }
+		}
+	}
 }

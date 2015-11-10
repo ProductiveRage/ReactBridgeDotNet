b4c6602eaadaaba0f77aa6d3c350bce558c36108
DanRoberts <DanRoberts@localhost>
2015-11-10 19:40:13 +0000

    Tried to reduce the example down to its most basic form - the SimpleExampleStore has multiple queryable state properties, rather than requiring its own view model, which means that the only user-action is a simple "MessageChangeAction" (for when the input box contents change). This makes this example simpler, I think, though more complicated apps may require a lot of specialised messages if every action required its own message type, so using Store-scoped view models may make more sense as apps grow.

---
+++ BridgeExamples/Actions/StoreInitialisedAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bridge.React;
+
+namespace BridgeExamples.Actions
+{
+    /// <summary>
+    /// If your application uses specialised Stores for various views / pages then the router component can send StoreInitialisedAction instances through the
+    /// Dispatcher, informing the appropriate Store that it needs to wake up (and allowing Stores for other views / pages to go to sleep). Note that in this
+    /// example application, there is no router and there is only a single Store, but this action is still useful in that it kicks everything off after all
+    /// of the dependencies have been initialised.
+    /// </summary>
+	public class StoreInitialisedAction : IDispatcherAction
+	{
+		public StoreInitialisedAction(object store)
+		{
+			if (store == null)
+				throw new ArgumentNullException("store");
+
+            Store = store;
+		}
+
+		/// <summary>
+		/// This will never be null
+		/// </summary>
+		public object Store { get; private set; }
+	}
+}
